95b36bf59a47bb0f7d1f46682e6b0aa490c6c6fb
mannycolon <colonmanuel7@gmail.com>
2020-11-19 11:50:33 -0500

    Properly Exporting components and new hook

--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,4 @@
-export { default as MarkdownComponent } from './MarkdownComponent'
 export { default as Card } from './Card'
+export { default as CardContent } from './CardContent'
+export { default as NotesSettingsCard } from './NotesSettingsCard'
 export { default as TsvContent } from './TsvContent'
