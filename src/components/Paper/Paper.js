43e9c4dea6812fb1706ea6230b903e7540bf6876
mannycolon <colonmanuel7@gmail.com>
2021-01-13 09:38:37 -0500

    Enhanced Paper styling

--- src/components/Paper/Paper.js
+++ src/components/Paper/Paper.js
@@ -4,13 +4,12 @@ import styled from 'styled-components'
 const Paper = styled.div`
   margin: 2.5px;
   padding: 16px;
-  background-color: #ffffff;
-  box-shadow: 0 14px 20px 2px rgba(0, 0, 0, 0.14),
-    0 6px 26px 5px rgba(0, 0, 0, 0.12), 0 8px 10px -5px rgba(0, 0, 0, 0.2);
-  border-radius: 4px;
+  border-radius: 2px;
   position: relative;
-  margin-bottom: 50px;
   transition: all 0.2s ease-in-out;
+  background-color: #ffffff;
+  box-shadow: 0 1px 1px 0 rgba(0, 0, 0, 0.14),
+    0 2px 1px -1px rgba(0, 0, 0, 0.12), 0 1px 3px 0 rgba(0, 0, 0, 0.14);
 `
 
 Paper.propTypes = {
