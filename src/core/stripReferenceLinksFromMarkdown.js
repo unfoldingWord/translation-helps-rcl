9898097226b18a587e240463a7e0a1b58e0f89bb
mannycolon <colonmanuel7@gmail.com>
2021-03-31 15:00:13 -0400

    Added support to strip [Matthew 1:17](../01/17.md) links

--- src/core/stripReferenceLinksFromMarkdown.js
+++ src/core/stripReferenceLinksFromMarkdown.js
@@ -15,5 +15,11 @@ export default function stripReferenceLinksFromMarkdown(markdown) {
   // tN: Convert all [<Bible Ref>](rc://<lang>/tn/*) links to just show the <Bible ref> (no link)
   markdown = markdown.replace(/\[([^\]]+)\]\(rc:\/\/[^/]+\/tn\/[^)]+\)/g, '$1')
 
+  // [Matthew 1:17](../01/17.md)
+  markdown = markdown.replace(
+    /\[([^\]]+)\]\(\.\.\/([0-9][0-9])\/([a-zA-Z0-9\s_\\.\-\(\):])+(.md)\)/gi,
+    '$1'
+  )
+
   return markdown
 }
