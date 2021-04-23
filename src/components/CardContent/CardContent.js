cd63369444fc114d9f16a69fa6c0e9155a631540
mannycolon <colonmanuel7@gmail.com>
2021-04-23 19:13:19 -0400

    Fixed horizontal scrolling when text overflows

--- src/components/CardContent/CardContent.js
+++ src/components/CardContent/CardContent.js
@@ -28,12 +28,12 @@ const CardContent = ({
     return <CircularProgress size={200} />
   } else if (errorMessage) {
     return (
-      <div style={{ fontSize: '1.3rem', height: '100%' }}>
-        <div
+      <div style={{ fontSize: '1.3rem', height: '100%', width: '100%' }}>
+        <p
           style={{
             fontSize,
             height: '100%',
-            display: 'flex',
+            display: 'grid',
             justifyContent: 'center',
             alignItems: 'center',
             fontWeight: 'bold',
@@ -41,7 +41,7 @@ const CardContent = ({
           }}
         >
           {errorMessage}
-        </div>
+        </p>
       </div>
     )
   } else if (markdown && typeof markdown === 'string') {
