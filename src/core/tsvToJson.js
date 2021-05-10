bbccdb903645529e5527d2e753f4a335644e66d8
mannycolon <colonmanuel7@gmail.com>
2021-05-10 16:11:25 -0400

    Clean-up

--- src/core/tsvToJson.js
+++ src/core/tsvToJson.js
@@ -1,19 +1,21 @@
+export default function tsvToJson(tsv) {
+  const result = []
 
-export default function tsvToJson(tsv){
-  const lines = tsv.trim().split("\n");
-  const result = [];
-  const headers = lines[0].split("\t");
+  if (tsv) {
+    const lines = tsv.trim().split('\n')
+    const headers = lines[0].split('\t')
 
-  for (let i = 1; i < lines.length; i++) {
-    const obj = {};
-    const currentline = lines[i].split("\t");
+    for (let i = 1; i < lines.length; i++) {
+      const obj = {}
+      const currentline = lines[i].split('\t')
 
-    for (let j = 0; j < headers.length; j++) {
-      obj[headers[j]] = currentline[j];
+      for (let j = 0; j < headers.length; j++) {
+        obj[headers[j]] = currentline[j]
+      }
+
+      result.push(obj)
     }
-
-    result.push(obj);
   }
 
-  return result;
+  return result
 }
