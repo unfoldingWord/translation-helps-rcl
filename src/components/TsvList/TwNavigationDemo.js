2c8cf3e061c8a12089b92a495fbae1de631c7724
Joel C Johnson <joelcjohnson123@gmail.com>
2023-03-31 13:39:38 +0530

    removed 4 spaces to 2

--- src/components/TsvList/TwNavigationDemo.js
+++ src/components/TsvList/TwNavigationDemo.js
@@ -5,145 +5,145 @@ import useContent from '../../hooks/useContent.js'
 import useCardState from '../../hooks/useCardState.js'
 
 const Card1 = ({ selectedQuote, setCurrentCheck, showSaveChangesPrompt }) => {
-    const viewMode = 'list'
-    const {
-        items,
-        markdown,
-        props: { languageId },
-    } = useContent({
-        verse: 2,
-        chapter: 1,
-        projectId: 'mat',
-        ref: 'master',
-        languageId: 'en',
-        resourceId: 'twl',
-        owner: 'test_org',
-        fetchMarkdown: false,
-        server: 'https://git.door43.org',
-        viewMode,
-    })
+  const viewMode = 'list'
+  const {
+    items,
+    markdown,
+    props: { languageId },
+  } = useContent({
+    verse: 2,
+    chapter: 1,
+    projectId: 'mat',
+    ref: 'master',
+    languageId: 'en',
+    resourceId: 'twl',
+    owner: 'test_org',
+    fetchMarkdown: false,
+    server: 'https://git.door43.org',
+    viewMode,
+  })
 
-    const {
-        state: { item, headers, filters, fontSize, itemIndex, markdownView },
-        actions: { setFilters, setFontSize, setItemIndex, setMarkdownView },
-    } = useCardState({
-        items,
-    })
+  const {
+    state: { item, headers, filters, fontSize, itemIndex, markdownView },
+    actions: { setFilters, setFontSize, setItemIndex, setMarkdownView },
+  } = useCardState({
+    items,
+  })
 
-    return (
-        <Card
-            items={items}
-            title={'translationWords List'}
-            headers={headers}
-            filters={filters}
-            fontSize={fontSize}
-            itemIndex={itemIndex}
-            setFilters={setFilters}
-            setFontSize={setFontSize}
-            setItemIndex={setItemIndex}
-            markdownView={markdownView}
-            setMarkdownView={setMarkdownView}
-            showSaveChangesPrompt={showSaveChangesPrompt}
-            disableFilters
-            disableNavigation
-            hideMarkdownToggle
-        >
-            <CardContent
-                item={item}
-                items={items}
-                filters={filters}
-                fontSize={fontSize}
-                markdown={markdown}
-                viewMode={'list'}
-                setCurrentCheck={setCurrentCheck}
-                languageId={languageId}
-                markdownView={markdownView}
-                selectedQuote={selectedQuote}
-                showSaveChangesPrompt={showSaveChangesPrompt}
-            />
-        </Card>
-    )
+  return (
+    <Card
+      items={items}
+      title={'translationWords List'}
+      headers={headers}
+      filters={filters}
+      fontSize={fontSize}
+      itemIndex={itemIndex}
+      setFilters={setFilters}
+      setFontSize={setFontSize}
+      setItemIndex={setItemIndex}
+      markdownView={markdownView}
+      setMarkdownView={setMarkdownView}
+      showSaveChangesPrompt={showSaveChangesPrompt}
+      disableFilters
+      disableNavigation
+      hideMarkdownToggle
+    >
+      <CardContent
+        item={item}
+        items={items}
+        filters={filters}
+        fontSize={fontSize}
+        markdown={markdown}
+        viewMode={'list'}
+        setCurrentCheck={setCurrentCheck}
+        languageId={languageId}
+        markdownView={markdownView}
+        selectedQuote={selectedQuote}
+        showSaveChangesPrompt={showSaveChangesPrompt}
+      />
+    </Card>
+  )
 }
 
 const Card2 = ({ selectedQuote, setCurrentCheck, showSaveChangesPrompt }) => {
-    const {
-        items,
-        markdown,
-        props: { languageId },
-    } = useContent({
-        verse: 2,
-        chapter: 1,
-        projectId: 'mat',
-        ref: 'master',
-        languageId: 'en',
-        resourceId: 'twl',
-        owner: 'test_org',
-        server: 'https://git.door43.org',
-    })
+  const {
+    items,
+    markdown,
+    props: { languageId },
+  } = useContent({
+    verse: 2,
+    chapter: 1,
+    projectId: 'mat',
+    ref: 'master',
+    languageId: 'en',
+    resourceId: 'twl',
+    owner: 'test_org',
+    server: 'https://git.door43.org',
+  })
 
-    const {
-        state: { item, headers, filters, fontSize, itemIndex, markdownView },
-        actions: { setFilters, setFontSize, setItemIndex, setMarkdownView },
-    } = useCardState({
-        items,
-        setCurrentCheck,
-        selectedQuote,
-    })
+  const {
+    state: { item, headers, filters, fontSize, itemIndex, markdownView },
+    actions: { setFilters, setFontSize, setItemIndex, setMarkdownView },
+  } = useCardState({
+    items,
+    setCurrentCheck,
+    selectedQuote,
+  })
 
-    return (
-        <Card
-            items={items}
-            title={'translationWords Article'}
-            headers={headers}
-            filters={filters}
-            fontSize={fontSize}
-            itemIndex={itemIndex}
-            setFilters={setFilters}
-            setFontSize={setFontSize}
-            setItemIndex={setItemIndex}
-            markdownView={markdownView}
-            setMarkdownView={setMarkdownView}
-            showSaveChangesPrompt={showSaveChangesPrompt}
-            disableFilters
-            hideMarkdownToggle
-        >
-            <CardContent
-                item={item}
-                items={items}
-                filters={filters}
-                fontSize={fontSize}
-                markdown={markdown}
-                viewMode={'markdown'}
-                setCurrentCheck={setCurrentCheck}
-                languageId={languageId}
-                markdownView={markdownView}
-                selectedQuote={selectedQuote}
-                showSaveChangesPrompt={showSaveChangesPrompt}
-            />
-        </Card>
-    )
+  return (
+    <Card
+      items={items}
+      title={'translationWords Article'}
+      headers={headers}
+      filters={filters}
+      fontSize={fontSize}
+      itemIndex={itemIndex}
+      setFilters={setFilters}
+      setFontSize={setFontSize}
+      setItemIndex={setItemIndex}
+      markdownView={markdownView}
+      setMarkdownView={setMarkdownView}
+      showSaveChangesPrompt={showSaveChangesPrompt}
+      disableFilters
+      hideMarkdownToggle
+    >
+      <CardContent
+        item={item}
+        items={items}
+        filters={filters}
+        fontSize={fontSize}
+        markdown={markdown}
+        viewMode={'markdown'}
+        setCurrentCheck={setCurrentCheck}
+        languageId={languageId}
+        markdownView={markdownView}
+        selectedQuote={selectedQuote}
+        showSaveChangesPrompt={showSaveChangesPrompt}
+      />
+    </Card>
+  )
 }
 
 export default function TwNavigationDemo() {
-    const [selectedQuote, setCurrentCheck] = useState(null)
-    const showSaveChangesPrompt = () => {
-        return new Promise((resolve, reject) => {
-            resolve()
-        })
-    }
+  const [selectedQuote, setCurrentCheck] = useState(null)
+  const showSaveChangesPrompt = () => {
+    return new Promise((resolve, reject) => {
+      resolve()
+    })
+  }
 
-    return (
-        <div style={{ display: 'flex' }}>
-            <Card1
-                selectedQuote={selectedQuote}
-                setCurrentCheck={setCurrentCheck}
-                showSaveChangesPrompt={showSaveChangesPrompt}
-            />
-            <Card2
-                selectedQuote={selectedQuote}
-                setCurrentCheck={setCurrentCheck}
-                showSaveChangesPrompt={showSaveChangesPrompt}
-            />
-        </div>
-    )
+  return (
+    <div style={{ display: 'flex' }}>
+      <Card1
+        selectedQuote={selectedQuote}
+        setCurrentCheck={setCurrentCheck}
+        showSaveChangesPrompt={showSaveChangesPrompt}
+      />
+      <Card2
+        selectedQuote={selectedQuote}
+        setCurrentCheck={setCurrentCheck}
+        showSaveChangesPrompt={showSaveChangesPrompt}
+      />
+    </div>
+  )
 }
--- src/hooks/useCardState.js
+++ src/hooks/useCardState.js
@@ -3,160 +3,160 @@ import useDeepCompareEffect from 'use-deep-compare-effect'
 import determineDiffTsvVersion from '../core/determineDiffTsvVersion'
 
 const useCardState = ({
-    id,
-    items,
-    verse,
-    chapter,
-    setCurrentCheck,
-    projectId,
-    resourceId,
-    selectedQuote = {},
-    useUserLocalStorage,
+  id,
+  items,
+  verse,
+  chapter,
+  setCurrentCheck,
+  projectId,
+  resourceId,
+  selectedQuote = {},
+  useUserLocalStorage,
 }) => {
-    const [itemIndex, setItemIndex] = useState(0)
-    const item = items ? items[itemIndex] : null
-    let initialHeaders = Object.keys(item || {})
-    initialHeaders = initialHeaders.filter(
-        item =>
-            item !== 'markdown' && item !== 'filePath' && item !== 'fetchResponse'
-    )
-    const [headers, setHeaders] = useUserLocalStorage
-        ? useUserLocalStorage(`headers_${id}`, [])
-        : useState([])
-    const [filters, setFilters] = useUserLocalStorage
-        ? useUserLocalStorage(`filters_${id}`, null)
-        : useState(null)
-    const [markdownView, setMarkdownView] = useUserLocalStorage
-        ? useUserLocalStorage(`markdownView${id}`, false)
-        : useState(false)
-    const [fontSize, setFontSize] = useUserLocalStorage
-        ? useUserLocalStorage(`fontSize_${id}`, 100)
-        : useState(100)
-    const { SupportReference, quote, occurrence } = selectedQuote || {}
+  const [itemIndex, setItemIndex] = useState(0)
+  const item = items ? items[itemIndex] : null
+  let initialHeaders = Object.keys(item || {})
+  initialHeaders = initialHeaders.filter(
+    item =>
+      item !== 'markdown' && item !== 'filePath' && item !== 'fetchResponse'
+  )
+  const [headers, setHeaders] = useUserLocalStorage
+    ? useUserLocalStorage(`headers_${id}`, [])
+    : useState([])
+  const [filters, setFilters] = useUserLocalStorage
+    ? useUserLocalStorage(`filters_${id}`, null)
+    : useState(null)
+  const [markdownView, setMarkdownView] = useUserLocalStorage
+    ? useUserLocalStorage(`markdownView${id}`, false)
+    : useState(false)
+  const [fontSize, setFontSize] = useUserLocalStorage
+    ? useUserLocalStorage(`fontSize_${id}`, 100)
+    : useState(100)
+  const { SupportReference, quote, occurrence } = selectedQuote || {}
 
-    useEffect(() => {
-        setItemIndex(0)
-        if (items?.length) {
-            setItem(0)
+  useEffect(() => {
+    setItemIndex(0)
+    if (items?.length) {
+      setItem(0)
+    }
+  }, [verse, chapter, projectId, items])
+
+  useEffect(() => {
+    if (items && typeof SupportReference === 'string') {
+      let index = items.findIndex(
+        ({
+          Quote,
+          TWLink,
+          OrigWords,
+          Occurrence,
+          SupportReference: itemSupportReference,
+        }) => {
+          // Support new TWL column headers (OrigWords & TWLink)
+          itemSupportReference = itemSupportReference || TWLink
+          Quote = Quote || OrigWords
+
+          return (
+            itemSupportReference?.includes(SupportReference) &&
+            quote === Quote &&
+            occurrence === Occurrence
+          )
         }
-    }, [verse, chapter, projectId, items])
+      )
 
-    useEffect(() => {
-        if (items && typeof SupportReference === 'string') {
-            let index = items.findIndex(
-                ({
-                    Quote,
-                    TWLink,
-                    OrigWords,
-                    Occurrence,
-                    SupportReference: itemSupportReference,
-                }) => {
-                    // Support new TWL column headers (OrigWords & TWLink)
-                    itemSupportReference = itemSupportReference || TWLink
-                    Quote = Quote || OrigWords
+      // When the quote or occurrence is changed in the twl we want to find the item index using the TWLink field
+      if (index == -1 && resourceId == 'tw') {
+        index = items.findIndex(
+          ({ TWLink, SupportReference: itemSupportReference }) => {
+            // Support new TWL column headers (TWLink)
+            itemSupportReference = itemSupportReference || TWLink
 
-                    return (
-                        itemSupportReference?.includes(SupportReference) &&
-                        quote === Quote &&
-                        occurrence === Occurrence
-                    )
-                }
-            )
+            return itemSupportReference?.includes(SupportReference)
+          }
+        )
+      }
 
-            // When the quote or occurrence is changed in the twl we want to find the item index using the TWLink field
-            if (index == -1 && resourceId == 'tw') {
-                index = items.findIndex(
-                    ({ TWLink, SupportReference: itemSupportReference }) => {
-                        // Support new TWL column headers (TWLink)
-                        itemSupportReference = itemSupportReference || TWLink
-
-                        return itemSupportReference?.includes(SupportReference)
-                    }
-                )
-            }
-
-            if (index >= 0 && index !== itemIndex) {
-                setItemIndex(index)
-            }
-        }
-    }, [items, SupportReference, quote, occurrence])
-
-    useDeepCompareEffect(() => {
-        if (headers?.length === 0) {
-            setHeaders(initialHeaders)
-        }
-    }, [item, headers])
-
-    useDeepCompareEffect(() => {
-        if (!filters && headers.length > 0) {
-            setFilters(headers)
-        }
-    }, [headers])
-
-    useDeepCompareEffect(() => {
-        if (
-            initialHeaders?.length &&
-            headers?.length &&
-            determineDiffTsvVersion(initialHeaders, headers)
-        ) {
-            // If different Tsv Version reset headers & filters.
-            setHeaders(initialHeaders)
-            setFilters(initialHeaders)
-        }
-    }, [item, headers])
-
-    function setItem(index) {
+      if (index >= 0 && index !== itemIndex) {
         setItemIndex(index)
-
-        if (items) {
-            let {
-                Quote,
-                OrigQuote,
-                OrigWords,
-                Occurrence,
-                SupportReference,
-                TWLink,
-                Reference,
-            } = items[index] || {}
-            // Support new TWL column headers (OrigWords & TWLink)
-            Quote = !Quote && OrigWords ? OrigWords : Quote
-            SupportReference = !SupportReference && TWLink ? TWLink : SupportReference
-            Quote = !Quote && OrigQuote ? OrigQuote : Quote
-
-            if (
-                setCurrentCheck &&
-                (Quote || OrigQuote) &&
-                Occurrence &&
-                typeof SupportReference === 'string'
-            ) {
-                setCurrentCheck({
-                    quote: Quote || OrigQuote,
-                    occurrence: Occurrence,
-                    SupportReference,
-                    reference: Reference,
-                })
-            } else if (setCurrentCheck) {
-                setCurrentCheck(null)
-            }
-        }
+      }
     }
+  }, [items, SupportReference, quote, occurrence])
 
-    return {
-        state: {
-            item,
-            headers,
-            fontSize,
-            itemIndex,
-            markdownView,
-            filters: filters || [],
-        },
-        actions: {
-            setFilters,
-            setFontSize,
-            setItemIndex: setItem,
-            setMarkdownView,
-        },
+  useDeepCompareEffect(() => {
+    if (headers?.length === 0) {
+      setHeaders(initialHeaders)
     }
+  }, [item, headers])
+
+  useDeepCompareEffect(() => {
+    if (!filters && headers.length > 0) {
+      setFilters(headers)
+    }
+  }, [headers])
+
+  useDeepCompareEffect(() => {
+    if (
+      initialHeaders?.length &&
+      headers?.length &&
+      determineDiffTsvVersion(initialHeaders, headers)
+    ) {
+      // If different Tsv Version reset headers & filters.
+      setHeaders(initialHeaders)
+      setFilters(initialHeaders)
+    }
+  }, [item, headers])
+
+  function setItem(index) {
+    setItemIndex(index)
+
+    if (items) {
+      let {
+        Quote,
+        OrigQuote,
+        OrigWords,
+        Occurrence,
+        SupportReference,
+        TWLink,
+        Reference,
+      } = items[index] || {}
+      // Support new TWL column headers (OrigWords & TWLink)
+      Quote = !Quote && OrigWords ? OrigWords : Quote
+      SupportReference = !SupportReference && TWLink ? TWLink : SupportReference
+      Quote = !Quote && OrigQuote ? OrigQuote : Quote
+
+      if (
+        setCurrentCheck &&
+        (Quote || OrigQuote) &&
+        Occurrence &&
+        typeof SupportReference === 'string'
+      ) {
+        setCurrentCheck({
+          quote: Quote || OrigQuote,
+          occurrence: Occurrence,
+          SupportReference,
+          reference: Reference,
+        })
+      } else if (setCurrentCheck) {
+        setCurrentCheck(null)
+      }
+    }
+  }
+
+  return {
+    state: {
+      item,
+      headers,
+      fontSize,
+      itemIndex,
+      markdownView,
+      filters: filters || [],
+    },
+    actions: {
+      setFilters,
+      setFontSize,
+      setItemIndex: setItem,
+      setMarkdownView,
+    },
+  }
 }
 
 export default useCardState
