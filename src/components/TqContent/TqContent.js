316466427c3c68e0e161c62f18bf084c854916d4
jincypjose <66897554+jincypjose@users.noreply.github.com>
2023-04-12 19:05:43 +0530

    Feature jincy 399 (#105)
    * merge feature
    
    * Added ability to merge from and to master in dcs
    
    * added logging for merge feature
    
    ---------
    
    Co-authored-by: kintsoogi <kintsoogii@proton.me>
    Co-authored-by: blm <mcleanb@photo-nomad.net>

--- src/components/TqContent/TqContent.js
+++ src/components/TqContent/TqContent.js
@@ -2,81 +2,81 @@ import React, { useState, useEffect } from 'react'
 import { BlockEditable } from 'markdown-translatable'
 import cleanMarkdownLineBreak from '../../core/cleanMarkdownLineBreak'
 
-export default function TsvContent({
-  item,
-  fontSize,
-  editable,
-  onTsvEdit,
-  markdownView,
+export default function TqContent({
+    item,
+    fontSize,
+    editable,
+    onTsvEdit,
+    markdownView,
 }) {
-  const [{ Question, Response }, setTqValue] = useState({
-    Question: null,
-    Response: null,
-  })
+    const [{ Question, Response }, setTqValue] = useState({
+        Question: null,
+        Response: null,
+    })
 
-  useEffect(() => {
-    if (Question || Response) {
-      const isOldTsvTq = item?.Annotation ? true : false
-      const newItem = { ...item }
+    useEffect(() => {
+        if (Question || Response) {
+            const isOldTsvTq = item?.Annotation ? true : false
+            const newItem = { ...item }
 
-      if (Question && Response && isOldTsvTq) {
-        newItem.Annotation = `${Question}> ${Response}`
-      }
+            if (Question && Response && isOldTsvTq) {
+                newItem.Annotation = `${Question}> ${Response}`
+            }
 
-      if (Question && newItem.Question) {
-        newItem.Question = Question
-      }
+            if (Question && newItem.Question) {
+                newItem.Question = Question
+            }
 
-      if (Response && newItem.Response) {
-        newItem.Response = Response
-      }
+            if (Response && newItem.Response) {
+                newItem.Response = Response
+            }
 
-      // Edit TSV item
-      onTsvEdit(newItem)
+            // Edit TSV item
+            onTsvEdit(newItem)
+        }
+    }, [Question, Response])
+
+    const handleEdit = (field, edit) => {
+        // Remove markdown that was added for view only
+        let newEdit = edit?.replace('#', '')
+        newEdit = cleanMarkdownLineBreak(newEdit)
+
+        setTqValue(prevState => ({
+            ...prevState,
+            [field]: newEdit?.trim(),
+        }))
     }
-  }, [Question, Response])
 
-  const handleEdit = (field, edit) => {
-    // Remove markdown that was added for view only
-    let newEdit = edit?.replace('#', '')
-    newEdit = cleanMarkdownLineBreak(newEdit)
+    let question, response
 
-    setTqValue(prevState => ({
-      ...prevState,
-      [field]: newEdit?.trim(),
-    }))
-  }
+    if (item.Annotation) {
+        const text = item.Annotation.replace('', '')
+        const chunks = text.split('?')
+        question = `${chunks[0]}?`
+        response = chunks[1].split('> ')[1]
+    } else {
+        question = `# ${item.Question}`
+        response = item.Response || ''
+    }
 
-  let question, response
-
-  if (item.Annotation) {
-    const text = item.Annotation.replace('', '')
-    const chunks = text.split('?')
-    question = `${chunks[0]}?`
-    response = chunks[1].split('> ')[1]
-  } else {
-    question = `# ${item.Question}`
-    response = item.Response || ''
-  }
-
-  return (
-    <div>
-      <BlockEditable
-        markdown={question}
-        editable={editable}
-        fontSize={fontSize}
-        preview={!markdownView}
-        onEdit={edit => handleEdit('Question', edit)}
-        style={{ display: 'block', padding: '0px' }}
-      />
-      <BlockEditable
-        markdown={response}
-        editable={editable}
-        fontSize={fontSize}
-        preview={!markdownView}
-        onEdit={edit => handleEdit('Response', edit)}
-        style={{ display: 'block', padding: '0px' }}
-      />
-    </div>
-  )
+    return (
+        <div>
+            <BlockEditable
+                markdown={question}
+                editable={editable}
+                fontSize={fontSize}
+                preview={!markdownView}
+                onEdit={edit => handleEdit('Question', edit)}
+                style={{ display: 'block', padding: '0px' }}
+            />
+            <BlockEditable
+                markdown={response}
+                editable={editable}
+                fontSize={fontSize}
+                preview={!markdownView}
+                onEdit={edit => handleEdit('Response', edit)}
+                style={{ display: 'block', padding: '0px' }}
+            />
+        </div>
+    )
 }
