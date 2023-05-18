8c1120518d089383b102e0efcacd35817e4930fb
kintsoogi <kintsoogii@proton.me>
2023-05-18 12:19:05 -0500

    Changed mergeMasterProps to only handle merge and loading

--- src/components/BranchMerger/hooks/useMasterMergeProps.js
+++ src/components/BranchMerger/hooks/useMasterMergeProps.js
@@ -1,108 +1,52 @@
-import { useEffect, useState, useMemo } from 'react'
+import { useEffect, useState } from 'react'
 
 export default function useMasterMergeProps({
   isLoading: _isLoading = false,
+  isSaving = false,
   useBranchMerger,
+  // bibleRef = {},
   reloadContent = null,
 } = {}) {
-  const [isLoading, setIsLoading] = useState(_isLoading);
-  const [isErrorDialogOpen,setIsErrorDialogOpen] = useState(false);
-  const [isMessageDialogOpen, setIsMessageDialogOpen] = useState(false);
+  const [isLoading, setIsLoading] = useState(_isLoading)
 
   const {
-    state: { mergeStatus, loadingMerge }, actions: { mergeMasterBranch, checkMergeStatus, checkUpdateStatus }
-  } = useBranchMerger;
-
-  const loadingProps = { color: loadingMerge ? "primary" : "secondary" };
+    state: { loadingMerge },
+    actions: { mergeMasterBranch, checkMergeStatus, checkUpdateStatus },
+  } = useBranchMerger
 
   useEffect(() => {
-    setIsLoading(false);
-    setIsMessageDialogOpen(false);
-  }, [targetFileHook.state])
-
-  // const { load: loadTargetFile } = targetFileHook.actions || {};
-  // const { content: targetContent } = targetFileHook.state || {};
-
-  // useEffect(() => {
-  //   checkMergeStatus();
-  // }, [targetContent,checkMergeStatus]);
-
-  const  { conflict, mergeNeeded, error, message, pullRequest } = mergeStatus
-  const pending = mergeNeeded || conflict
-  const blocked = conflict || error;
-
-  const {message: dialogMessage, title: dialogTitle, link: dialogLink} = useMemo(() => {
-    if (conflict) return {
-      title: "Conflict Error",
-      message: "It appears that someone has merged changes that conflict with your current merge request. Please contact your administrator.",
-      link: pullRequest
-    };
-    if (error && message) return {
-      title: "Error",
-      message,
-      link: pullRequest
-    };
-    if (error && !message) return {
-      title: "Unknown error.",
-      message: "Contact your administrator.",
-      link: pullRequest
-    };
-    if (!mergeNeeded) return  {
-      title: "Up-to-date",
-      message: "Your content is already up-to-date",
-      link: ""
-    };
-    return {
-      title: "Unknown state.",
-      message: "Contact your administrator.",
-      link: pullRequest
-    };
-  }, [message, conflict, mergeNeeded, error, pullRequest])
-
-  const dialogLinkTooltip = "Pull-Request URL"
-
-  const onClick = () => {
-    if (blocked | !pending) return setIsErrorDialogOpen(true)
-    setIsMessageDialogOpen(true);
-  }
-
-  const onCloseErrorDialog = () => {
-    setIsErrorDialogOpen(false)
-  }
-
-  const onCancel = () => {
-    setIsMessageDialogOpen(false);
-  }
-
-  const onSubmit = (description) => {
-    setIsLoading(true);
-    mergeMasterBranch(description).then((response) => {
-      if (response.success && response.message === "") {
-        reloadContent && reloadContent()
-        checkUpdateStatus()
-      }
-      else {
-        setIsErrorDialogOpen(true);
+    if (isSaving & !isLoading) {
+      setIsLoading(true)
+    }
+    if (!isSaving & isLoading) {
+      // There is a race condition with server returning
+      // a conflict while processing the last commit
+      // the setTimeout tries to make sure we don't get a false conflict
+      setTimeout(() => {
+        checkMergeStatus().then(status => {
+          if (status.conflict) checkMergeStatus()
+        })
         setIsLoading(false)
-        setIsMessageDialogOpen(false);
-      };
+      }, 1000)
+    }
+  }, [isSaving, isLoading, checkMergeStatus])
+
+  const callMergeUserBranch = async description => {
+    setIsLoading(true)
+    mergeMasterBranch(description).then(response => {
+      if (response.success && response.message === '') {
+        reloadContent?.()
+        checkUpdateStatus()
+        return true
+      } else {
+        setIsLoading(false)
+        return false
+      }
     })
   }
 
   return {
-    isLoading: (isLoading | loadingMerge),
-    onClick,
-    onSubmit,
-    onCancel,
-    onCloseErrorDialog,
-    isMessageDialogOpen,
-    isErrorDialogOpen,
-    dialogMessage,
-    dialogTitle,
-    dialogLink,
-    dialogLinkTooltip,
-    pending,
-    blocked,
-    loadingProps
+    isLoading: isLoading | loadingMerge,
+    callMergeUserBranch,
   }
 }
