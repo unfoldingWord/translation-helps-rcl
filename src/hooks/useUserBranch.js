08762dfd22a3319453f9ba7325895dd548c433cb
Bruce McLean <mcleanb@photo-nomad.net>
2023-06-05 15:28:07 -0400

    Merge 2.0.0 into main (#112)
    * add support for user/book branch
    
    * cleanup
    
    * cleanup
    
    * cleanup
    
    * fix for branch name
    
    * fix for branch name
    
    * bump version
    
    * fixes for book edit branch
    
    * fixes for book edit branch
    
    * added id's for minimize and save button (#108)
    
    * added id's for minimize and save button
    
    * updated the version
    
    * remove logic to check for book change - now look for re-check flag
    
    * return flag that branch determined
    
    * fixes
    
    * cleanup
    
    * fix for user edit branch
    
    * bump version
    
    * bump version
    
    * comments
    
    * bump version
    
    * cleanup
    
    * fix crash on old tsv format
    
    * fix crash on old tsv format
    
    * fix for reference ranges
    
    * fix for old tsv format
    
    * make sure all copies of a reference range are updated with latest data
    
    * hide _referenceRange on tN
    
    * fixes for creating user branch for resources
    
    * clean up
    
    * clean up
    
    ---------
    
    Co-authored-by: jincypjose <66897554+jincypjose@users.noreply.github.com>
    Co-authored-by: Joel C Johnson <joelcjohnson123@gmail.com>

--- src/hooks/useUserBranch.js
+++ src/hooks/useUserBranch.js
@@ -1,209 +1,277 @@
-import { useState } from 'react'
+import {useEffect, useState} from 'react'
 import {
-    createUserBranch,
-    getUserEditBranch,
-    getUsersWorkingBranch,
-    processHttpErrors,
-    processUnknownError,
+  createUserBranch,
+  getUserEditBranch,
+  getUsersWorkingBranch,
+  processHttpErrors,
+  processUnknownError,
 } from '../core'
 import useDeepCompareEffect from 'use-deep-compare-effect'
 
 /**
  * manage edit branch for card
- * @param {string} languageId
- * @param {string} server
- * @param {string} owner
- * @param {string} ref
- * @param {function} useUserLocalStorage
- * @param {string} loggedInUser
+ * @param {string} appRef - branch or version
  * @param {object} authentication
- * @param {string} cardResourceId - resource id for this card
- * @param {string} cardId - id for the card
+ * @param {string} bookId - optional for book branch (such as `php`), otherwise we just use a user edit branch
+ * @param {string} cardResourceId - resource id for this card such as `ult`
+ * @param {string} cardId - id for the card such as `scripture_card_Literal_Translation`
+ * @param {number} checkForEditBranch for every change of value, re-check for edit branch
+ * @param {string} languageId
+ * @param {string} loggedInUser
  * @param {function} onResourceError - callback function for error fetching resource
+ * @param {string} owner
+ * @param {string} server
+ * @param {function} useUserLocalStorage
  * @return {{state: {editing: boolean}, actions: {startEdit: ((function(): Promise<void>)|*), saveEdit: ((function(*): Promise<void>)|*)}}}
  */
 const useUserBranch = ({
-    owner,
-    server,
     appRef,
+    authentication,
+    bookId,
     cardId,
+    cardResourceId,
+    checkForEditBranch,
     languageId,
     loggedInUser,
-    authentication,
-    cardResourceId,
     onResourceError,
-    useUserLocalStorage,
+    owner,
+    server,
 }) => {
-    // initialize to default for app
-    const [ref, setRef] = useUserLocalStorage(`${cardId}_ref`, appRef)
-    const [usingUserBranch, setUsingUserBranch] = useUserLocalStorage
-        ? useUserLocalStorage(`editing_${cardId}_${languageId}`, false)
-        : useState(false)
-    const [listRef, setListRef] = useState(ref)
-    const [contentRef, setContentRef] = useState(ref)
-    const userEditBranchName = loggedInUser ? getUserEditBranch(loggedInUser) : null;
+  // initialize to default for app
+  const [state, _setState] = useState({
+    branchDetermined: false,
+    contentRef: appRef,
+    fetchingBranch: false,
+    lastFetch: null,
+    listRef: appRef,
+    ref: appRef,
+    usingUserBranch: false,
+  })
+  const {
+    branchDetermined,
+    contentRef,
+    fetchingBranch,
+    lastFetch,
+    listRef,
+    ref,
+    usingUserBranch,
+  } = state
+  const userEditBranchName = loggedInUser ? getUserEditBranch(loggedInUser, bookId) : null;
 
-    async function getWorkingBranchForResource(resourceId) {
-        const repoName = `${languageId}_${resourceId}`
-        let currentBranch
+  function setState(newState) {
+    _setState(prevState => ({ ...prevState, ...newState }))
+  }
 
-        try {
-            currentBranch = await getUsersWorkingBranch(
-                server,
-                owner,
-                repoName,
-                userEditBranchName
-            )
-        } catch (e) {
-            console.error(
-                `useUserBranch - get user branch FAILED ${JSON.stringify({
-                    server,
-                    owner,
-                    repoName,
-                    loggedInUser,
-                })}`,
-                e
-            )
-            processHttpErrors(
-                e?.response || null,
-                `${repoName} user edit branch`,
-                e?.url,
-                onResourceError
-            )
-        }
-        return currentBranch
+  async function getWorkingBranchForResource(resourceId) {
+    const repoName = `${languageId}_${resourceId}`
+    let currentBranch
+
+    try {
+      currentBranch = await getUsersWorkingBranch(
+        server,
+        owner,
+        repoName,
+        userEditBranchName
+      )
+    } catch (e) {
+      console.error(
+        `useUserBranch - get user branch FAILED ${JSON.stringify({
+          server,
+          owner,
+          repoName,
+          loggedInUser,
+        })}`,
+        e
+      )
+      processHttpErrors(
+        e?.response || null,
+        `${repoName} user edit branch`,
+        e?.url,
+        onResourceError
+      )
     }
+    return currentBranch
+  }
 
-    /**
-     * makes sure that we are using the user's edit branch, if not it creates one for user
-     * @return {Promise<boolean>} returns true if user branch already exists or created
-     */
-    async function ensureUserEditBranch() {
-        const repoName = `${languageId}_${cardResourceId}`
+  /**
+   * makes sure that we are using the user's edit branch, if not it creates one for user
+   * @return {Promise<boolean>} returns true if user branch already exists or created
+   */
+  async function ensureUserEditBranch() {
+    const repoName = `${languageId}_${cardResourceId}`
 
-        if (authentication) {
-            const config = authentication.config
+    if (authentication) {
+      const config = authentication.config
 
-            try {
-                if (ref !== userEditBranchName) {
-                    const response = await createUserBranch(
-                        server,
-                        owner,
-                        repoName,
-                        config,
-                        userEditBranchName
-                    )
-                    console.info(
-                        `useUserBranch - user branch created ${JSON.stringify({
-                            server,
-                            owner,
-                            repoName,
-                            loggedInUser,
-                        })}`,
-                        response
-                    )
-
-                    setRef(userEditBranchName) // switch current branch to user edit branch
-                }
-                return userEditBranchName
-            } catch (e) {
-                console.error(`useUserBranch - ensureUserEditBranch FAILED`, e)
-                processUnknownError(
-                    e,
-                    'DCS API',
-                    `create user branch ${userEditBranchName} on ${repoName}`,
-                    onResourceError
-                )
-            }
-        }
-        return false
-    }
-
-    /**
-     * called to make sure user edit branch exists and that we are switched to that branch
-     * @return {Promise<void>}
-     */
-    async function startEdit() {
-        if (!usingUserBranch) {
-            const branch = await ensureUserEditBranch()
-            if (branch) {
-                setUsingUserBranch(true)
-                return branch
-            }
-        }
-
-        return false
-    }
-
-    /**
-     * update ref value if different
-     * @param {any} ref
-     * @param {any} newRef
-     * @param {function} setRefState
-     */
-    function updateRef(ref, newRef, setRefState) {
-        newRef = newRef || 'master' // default to master in case error fetching branch name
-        if (ref !== newRef) {
-            setRefState(newRef)
-        }
-    }
-
-    useDeepCompareEffect(() => {
-        const updateStatus = async () => {
-            let newListRef, newContentRef
-            const currentResourceRef = await getWorkingBranchForResource(cardResourceId)
-
-            // TRICKY: in the case of tWords there are two repos (tw for articles and twl for word list) and each one may have different branch
-            switch (cardResourceId) {
-                case 'tw':
-                    newContentRef = currentResourceRef
-                    newListRef = await getWorkingBranchForResource('twl')
-                    break
-
-                case 'twl':
-                    newListRef = currentResourceRef
-                    newContentRef = await getWorkingBranchForResource('tw')
-                    break
-
-                default:
-                    newListRef = newContentRef = currentResourceRef
-            }
-
-            // update states
-            if (currentResourceRef !== ref) {
-                setRef(currentResourceRef)
-            }
-
-            setUsingUserBranch(currentResourceRef === userEditBranchName) // if edit branch may have been merged or deleted, we are no longer using edit branch
-            updateRef(listRef, newListRef, setListRef)
-            updateRef(contentRef, newContentRef, setContentRef)
-        }
-        if (loggedInUser) {
-            updateStatus().catch(console.error)
-        }
-    }, [
-        {
-            ref,
-            languageId,
+      try {
+        if (ref !== userEditBranchName) {
+          const response = await createUserBranch(
             server,
             owner,
-            loggedInUser,
-            usingUserBranch,
-        }
-    ])
+            repoName,
+            config,
+            userEditBranchName
+          )
+          console.log(
+            `useUserBranch - user branch created ${JSON.stringify({
+              server,
+              owner,
+              repoName,
+              loggedInUser,
+            })}`,
+            response
+          )
 
-    return {
-        state: {
-            listRef,
-            contentRef,
-            usingUserBranch,
-            userEditBranchName,
-            workingResourceBranch: ref,
-        },
-        actions: {
-            startEdit,
-        },
+          setState( { ref: userEditBranchName }) // switch current branch to user edit branch
+        } else {
+          console.log(
+            `useUserBranch - already using user branch ${JSON.stringify({
+              server,
+              owner,
+              repoName,
+              loggedInUser,
+            })}`
+          )
+        }
+        return userEditBranchName
+      } catch (e) {
+        console.error(`useUserBranch - ensureUserEditBranch FAILED`, e)
+        processUnknownError(
+          e,
+          'DCS API',
+          `create user branch ${userEditBranchName} on ${repoName}`,
+          onResourceError
+        )
+      }
     }
+    return false
+  }
+
+  /**
+   * called to make sure user edit branch exists and that we are switched to that branch
+   * @return {Promise<void>}
+   */
+  async function startEdit() {
+    if (!usingUserBranch) {
+      const branch = await ensureUserEditBranch()
+      if (branch) {
+        let newListRef = listRef, newContentRef = contentRef
+        switch (cardResourceId) {
+          case 'tw':
+            newContentRef = branch
+            break
+
+          case 'twl':
+            newListRef = branch
+            break
+
+          default:
+            newListRef = newContentRef = branch
+        }
+
+        setState( {
+          contentRef: newContentRef,
+          listRef: newListRef,
+          ref: branch,
+          usingUserBranch: true,
+        })
+        return branch
+      } else {
+        console.warn(`useUserBranch.startEdit - failed to create user branch`)
+        return null
+      }
+    }
+
+    return userEditBranchName
+  }
+
+  useDeepCompareEffect(() => {
+    const updateStatus = async () => {
+      let newListRef, newContentRef
+      const fetching = JSON.stringify( {
+        bookId,
+        cardResourceId,
+        languageId,
+        loggedInUser,
+        owner,
+        appRef,
+        server,
+      })
+      if (fetchingBranch && (fetching === lastFetch)) {
+        console.log(`updateStatus() - already fetching`, fetching)
+      } else if (fetching !== lastFetch) {
+        setState( {
+          fetchingBranch: true,
+          lastFetch: fetching,
+          usingUserBranch: false,
+          branchDetermined: false,
+        })
+        const currentResourceRef = await getWorkingBranchForResource(cardResourceId)
+        console.log(`updateStatus() - `, fetching)
+
+        // TRICKY: in the case of tWords there are two repos (tw for articles and twl for word list) and each one may have different branch
+        switch (cardResourceId) {
+          case 'tw':
+            newContentRef = currentResourceRef
+            newListRef = await getWorkingBranchForResource('twl')
+            break
+
+          case 'twl':
+            newListRef = currentResourceRef
+            newContentRef = await getWorkingBranchForResource('tw')
+            break
+
+          default:
+            newListRef = newContentRef = currentResourceRef
+        }
+
+        // update states
+        if (currentResourceRef !== ref) {
+          console.log(`updateStatus() - changing ref`, { cardResourceId, ref, currentResourceRef })
+        }
+        setState( {
+          branchDetermined: true,
+          contentRef: newContentRef,
+          fetchingBranch: false,
+          listRef: newListRef,
+          ref: currentResourceRef,
+          usingUserBranch: currentResourceRef === userEditBranchName,
+        })
+        console.log(`updateStatus() - branch determined`, { cardResourceId, ref, currentResourceRef })
+      } else {
+        console.log(`updateStatus() - already fetched`, fetching)
+        setState( {
+          branchDetermined: true,
+          fetchingBranch: false,
+        })
+      }
+    }
+    if (loggedInUser) {
+      updateStatus().catch(console.error)
+    }
+  }, [
+    {
+      appRef,
+      cardResourceId,
+      checkForEditBranch,
+      languageId,
+      loggedInUser,
+      owner,
+      server,
+    }
+  ])
+
+  return {
+    state: {
+      contentRef,
+      branchDetermined,
+      listRef,
+      userEditBranchName,
+      usingUserBranch,
+      workingResourceBranch: ref,
+    },
+    actions: { startEdit },
+  }
 }
 
 export default useUserBranch
