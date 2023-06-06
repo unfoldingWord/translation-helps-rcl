fa1fffdae8c3b7e62fa0e7a5c7912be209d455f6
Noah Harvey <noah.harvey247@gmail.com>
2023-06-06 09:57:06 -0400

    performs cleanups in glBible.js

--- src/core/glBible.js
+++ src/core/glBible.js
@@ -1,24 +1,27 @@
 /**
  * @typedef {string} ResourceLink a valid url path pointing to a Bible resource
- * @typedef {string} GlBibleRepoName the name of a glBible (e.g: "hi_gst")
- * @param {GlBibleRepoName} glBible
+ * @typedef {string} GlBible the name of a glBible (e.g: "hi_gst")
+ */
+
+/** 
+ * @param {GlBible} glBible
  * @param {string} owner the owner of a repository (e.g: 'Door43-Catalog')
  * @returns {ResourceLink} the resource link for the glBible
  * @todo TEST!
  */
-export const glBibleToResourceLink = (owner, glBible) => `${owner}/${glBibleLangId(glBible)}/${glBibleBible(glBible)}/master`;
+export const resourceLink = (owner, glBible) => `${owner}/${langId(glBible)}/${bibleName(glBible)}/master`;
 
 /**
- * @param {GlBibleRepoName} glBible
- * @returns {undefined|string} the language id of the glBible
+ * @param {GlBible} glBible
+ * @returns {undefined | string} the language id of the glBible
  * @todo TEST!
  */
-export const glBibleLangId = (glBible) => glBible.split('_')[0];
+export const langId = (glBible) => glBible.split('_')[0];
 
 /**
  *
- * @param {GlBibleRepoName} glBible
- * @returns {undefined | string} the bible of the glBible
+ * @param {GlBible} glBible
+ * @returns {undefined | string} the bible name of the glBible
  * @todo TEST!
  */
-export const glBibleBible = (glBible) => glBible.split('_')[1];
+export const bibleName = (glBible) => glBible.split('_')[1];
